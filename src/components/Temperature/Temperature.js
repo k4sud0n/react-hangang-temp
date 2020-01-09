47831c0f19de07615f8503c0bad31e8fb7e8137c
Sihyun Kim <legitcode267@gmail.com>
2020-01-09 14:01:42 -0700

    Finish Project

--- src/components/Temperature/Temperature.js
+++ src/components/Temperature/Temperature.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import * as api from '../../lib/api';
+import './Temperature.css';
 
 class Temperature extends Component {
 
@@ -38,11 +39,36 @@ class Temperature extends Component {
 
     render() {
 
-        const { temperature, time } = this.state;
+        const { temperature, time, loading } = this.state;
 
-        return (
-            <div>{temperature} / {time}</div>
-        )
+        if(loading) {
+            return (
+                <div id="content">
+                    <p id="title">Hangang River Temperature</p>
+                    <br />
+                    <br />
+                    <p id="subtitle">현재 한강물의 온도는</p>
+                    <p id="temp">Loading...</p>
+                    <br />
+                    <br />
+                    <p id="foo2">Loading...</p>
+                </div>
+            )
+        } else {
+            return (
+                <div id="content">
+                    <p id="title">Hangang River Temperature</p>
+                    <br />
+                    <br />
+                    <p id="subtitle">현재 한강물의 온도는</p>
+                    <p id="temp"><span role="img" aria-label="droplet">💧</span> {temperature}</p>
+                    <br />
+                    <br />
+                    <p id="foo2">{time} 에 측정된 자료.</p>
+                </div>
+            )
+        }
+        
     }
 }
 
