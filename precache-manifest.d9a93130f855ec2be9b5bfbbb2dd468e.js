75b0295098613a94e05a8dede2efc7b428818206
Sihyun Kim <legitcode267@gmail.com>
2020-01-09 14:05:16 -0700

    Updates

---
+++ precache-manifest.d9a93130f855ec2be9b5bfbbb2dd468e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfcfef64fec8a176a3c8d99b6fe38c2",
+    "url": "/react-hangang-temp/index.html"
+  },
+  {
+    "revision": "e4fa2188d23b01956622",
+    "url": "/react-hangang-temp/static/css/main.6c3c9d0a.chunk.css"
+  },
+  {
+    "revision": "f8e765c557d2a07ca7de",
+    "url": "/react-hangang-temp/static/js/2.89368e58.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/react-hangang-temp/static/js/2.89368e58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4fa2188d23b01956622",
+    "url": "/react-hangang-temp/static/js/main.5c247913.chunk.js"
+  },
+  {
+    "revision": "59c83a6cc8eef871b0bd",
+    "url": "/react-hangang-temp/static/js/runtime-main.929bbb9c.js"
+  },
+  {
+    "revision": "d48cea5ac3a3367e11d9045bad956b1b",
+    "url": "/react-hangang-temp/static/media/background.d48cea5a.jpg"
+  }
+]);
